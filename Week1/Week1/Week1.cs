b42e703c352ea89968c36d7c634e9f5c64238282
Jibreel Muhammad <cntr.management@outlook.com>
2024-07-10 20:35:11 -0500

    New Defintions

--- Week1/Week1/Week1.cs
+++ Week1/Week1/Week1.cs
@@ -19,11 +19,13 @@ namespace Week1
 
             //Integer: Whole numbers. 32-bit values go up to 2,147,483,647. The equation is (2^32/ 2) -1 = 2,147,483,647
             //int.MaxValue; Max value of an integer.
-            Console.Writeline($"Max Integer Value:{int.MaxValue}");
+
+            Console.WriteLine($"Max Integer Value:{int.MaxValue}");
 
             //Declare without initial assignment
             int num; //All statements must end with a semi colomn;
-            Console.Writeline($"Max Integer Value:{int.MaxValue}");
+
+            Console.WriteLine($"Max Integer Value:{int.MaxValue}");
 
 
             //int64.MaxValue = 64-bit Integers can be accessed with int.64 struct or 
@@ -56,9 +58,9 @@ namespace Week1
             bool boolfalse = false;
 
             //Characters: Single ASCII or UTF-8 value ( Depending on charater set used, these are the most common character sets)
-
-            char lowerA = "a";
-            char upperA = "A";
+            //For char we use '' single quotes not ("")
+            char lowerA = 'a';
+            char upperA = 'A';
 
             int lowerAValue = (int)lowerA;
             Console.WriteLine($"Integer value of lower-casea:{lowerA}");
@@ -71,13 +73,21 @@ namespace Week1
             char[] helloWorldCharArray = helloWorld.ToCharArray();
             // anything yellow is a method.
 
+
+
             // User Input 
-            Console.WriteLine();
-            
-            Console.Read();
+            Console.WriteLine();// Will create a new line under text
 
 
+            Console.Write("Enter your name"); // Console.Write will not create a new line stops at the text
+            // We will declare a string variable called "name" and assign the user inout to the string.
 
+            string name = Console.ReadLine();
+            // Will create the string variable name and then await keyboard input from user iin the console. after taking inthe user input from the keyboard
+            // we will print back the text the user typed
+
+            Console.WriteLine($"Your name is {name}");
+            Console.ReadKey(); // Keep console window open until key is pressed end to program 
 
 
 
