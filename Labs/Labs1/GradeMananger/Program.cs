c77cdc89c296a05f79958b5b07e6372461cf1926
Jibreel Muhammad <cntr.management@outlook.com>
2024-07-12 20:04:55 -0500

    New string Operation

--- Labs/Labs1/GradeMananger/Program.cs
+++ Labs/Labs1/GradeMananger/Program.cs
@@ -7,10 +7,21 @@ using System.Threading.Tasks;
 namespace GradeMananger
 {
     public class Program
+        // Global Scope
     {
         public static void Main(string[] args)
-            // commit often and commit early
+            //Commit MOTO: commit often and commit early
+            // Local Scope
         {
+            string applicationName = "GradeManager";
+
+            Console.WriteLine(applicationName);
+
+            Console.ReadKey();
+
+
+
         }
+
     }
 }
