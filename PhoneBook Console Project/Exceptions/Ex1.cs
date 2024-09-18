8147165baaf40ee1aaf77dc7c760c72fbc38dae0
Jibreel Muhammad <cntr.management@outlook.com>
2024-09-18 18:10:45 -0500

    New method updates

---
+++ PhoneBook Console Project/Exceptions/Ex1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook_Console_Project.Exceptions
+{
+    internal class Ex1 : Exception
+    {
+
+    
+        // Default Constructor
+        public Ex1()
+        {
+
+        }
+
+        //Custom exception message for InvalidPhoneNumberException
+
+        public Ex1(string message) : base(message)
+        {
+
+        }
+
+        public Ex1(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    
+    }
+}
